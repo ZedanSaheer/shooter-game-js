255149a55cb3cf6111bfa900af2462628eb66938
Zedan Saheer <zed.saheer5@gmail.com>
2022-01-09 02:35:50 +0530

    Creating basic game components

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const startButton = document.getElementById("start");
 const menu = document.getElementById("menu");
+const info = document.getElementById("info");
+const quitButton = document.getElementById("quit")
+
+//Game menu functions
 
 const startNewGame = () => {
-    if (startButton.innerText === "START NEW GAME") {
-        menu.classList.add("disable_menu")
-        startButton.innerText = "QUIT"
-    }
+    menu.classList.add("disable_menu");
+    startButton.classList.add("disable_menu");
+    quitButton.style.display="block"
 }
 
 const showInfo = () => {
-    
+    info.classList.remove("disable_menu");    
 }
+
+const closeInfo = () => {
+    info.classList.add("disable_menu");
+}
+
+const quitGame = () => {
+    menu.classList.remove("disable_menu");
+    startButton.classList.remove("disable_menu");
+    quitButton.style.display="none"
+} 
+
+//Game movement and action functions
