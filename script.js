a142b83d75d57cf81f7c6775fbc2d771daf37d63
Zedan Saheer <zed.saheer5@gmail.com>
2022-01-10 03:06:59 +0530

    Added shooting logic with health progress

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const playerTwo = document.getElementById("two");
 const healthContainer = document.getElementById("health");
 const playerOneScore = document.getElementById("playerOneScore");
 const playerTwoScore = document.getElementById("playerTwoScore");
+const bulletOne = document.getElementById("bullet_one");
+const bulletTwo = document.getElementById("bullet_two");
 
 
 //Game menu functions
@@ -14,18 +16,18 @@ const playerTwoScore = document.getElementById("playerTwoScore");
 const startNewGame = () => {
     menu.classList.add("disable_menu");
     startButton.classList.add("disable_menu");
-    quitButton.style.display="block";
+    quitButton.style.display = "block";
     playerOne.classList.remove("disable_menu");
     playerTwo.classList.remove("disable_menu");
     healthContainer.classList.remove("disable_menu");
-    if(playerOneScore!==0 || playerTwoScore !==0){
-        playerOneScore.innerText=0;
-        playerTwoScore.innerText=0;
+    if (playerOneScore !== 0 || playerTwoScore !== 0) {
+        playerOneScore.innerText = 0;
+        playerTwoScore.innerText = 0;
     }
 }
 
 const showInfo = () => {
-    info.classList.remove("disable_menu");    
+    info.classList.remove("disable_menu");
 }
 
 const closeInfo = () => {
@@ -35,25 +37,35 @@ const closeInfo = () => {
 const quitGame = () => {
     menu.classList.remove("disable_menu");
     startButton.classList.remove("disable_menu");
-    quitButton.style.display="none";
+    quitButton.style.display = "none";
     playerOne.classList.add("disable_menu");
     playerTwo.classList.add("disable_menu");
     healthContainer.classList.add("disable_menu");
-    if(playerOneScore!==0 || playerTwoScore !==0){
-        playerOneScore.innerText=0;
-        playerTwoScore.innerText=0;
+    if (playerOneScore !== 0 || playerTwoScore !== 0) {
+        playerOneScore.innerText = 0;
+        playerTwoScore.innerText = 0;
     }
-} 
+}
 
 //Game movement and action functions
 
-const jumpOnKeyDown = (e) => {
-   if(e.key==="w"){
-       playerOne.classList.toggle("jump");
-   }
-   if(e.key==="ArrowUp"){
-       playerTwo.classList.toggle("jump");
-   }
+const shootOnClickOrSpace = async (e) => {
+    if (e.key === "s") {
+        bulletOne.classList.toggle("player_one_shoot");
+    }
+    if (e.key==="ArrowDown"){
+        bulletTwo.classList.toggle("player_two_shoot");
+    }
 }
 
-document.addEventListener("keydown",jumpOnKeyDown);
+const jumpOnKeyDown = (e) => {
+    if (e.key === "w") {
+        playerOne.classList.toggle("jump");
+    }
+    if (e.key === "ArrowUp") {
+        playerTwo.classList.toggle("jump");
+    }
+}
+
+document.addEventListener("keydown", shootOnClickOrSpace);
+document.addEventListener("keydown", jumpOnKeyDown);
